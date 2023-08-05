03a4ab49091f084e5213ca51b79b5fe1d9779c7a
franky-lim24 <franky.2021@scis.smu.edu.sg>
2023-08-05 14:49:06 +0800

    Revert "test workflow"
    This reverts commit aca8618be8aa87cdaf4e459beb3fe6aa047f3b19.

--- main.py
+++ main.py
@@ -3,19 +3,19 @@ import unittest
 class Calculator:
   # TODO: Write your methods here
   def add(self, a, b):
-    return a+b
+    return 0
   
   # TODO: Write your methods here
   def subtract(self, a, b):
-    return a-b
+    return 0
   
   # TODO: Write your methods here
   def multiply(self, a, b):
-    return a*b
+    return 0
     
   # TODO: Write your methods here
   def divide(self, a, b):
-    return a/b
+    return 0
 
 # Test Cases
 class TestAdd(unittest.TestCase):
