9cf74591b2dffce80731fc95fb72d93627dfe4f5
Jonathan-Teo <teo.junrui.jonathanedward2@gmail.com>
2023-08-05 16:38:56 +0800

    Added instructions for subtract and divide method

--- Calculator.py
+++ Calculator.py
@@ -4,7 +4,7 @@ class Calculator:
     return 0
   
   # TODO: Write your methods here
-  def subtract(self, a, b):
+  def subtract(self, a, b): ## subtracts b from a
     return 0
   
   # TODO: Write your methods here
@@ -12,5 +12,5 @@ class Calculator:
     return 0
     
   # TODO: Write your methods here
-  def divide(self, a, b):
+  def divide(self, a, b): ## divides a by b 
     return 0
